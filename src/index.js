4fd7950536866cc740ac9b30085b7ad0911d0b46
Ashley Marie Laing <ashleym.laing@gmail.com>
2017-12-30 17:49:40 -0500

    understanding the output management of webpack

--- src/index.js
+++ src/index.js
@@ -10,21 +10,26 @@
 // document.body.appendChild(component());
 
 import _ from 'lodash';
-import './style.css';
-import Icon from './twitter.jpg';
+// import './style.css';
+// import Icon from './twitter.jpg';
+// import Data from './data.xml';
+
+
 function component() {
   var element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // element.classList.add('hello');
 
      // Add the image to our existing div.
-   var myIcon = new Image();
-   myIcon.src = Icon;
+   // var myIcon = new Image();
+   // myIcon.src = Icon;
+   //
+   // element.appendChild(myIcon);
 
-   element.appendChild(myIcon);
+   // console.log(Data);
 
   return element;
 }
