d636dce5560ce25da2d63960852b55a5511c2f26
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-01-04 10:42:47 -0500

    made progress understanding webpack

--- src/index.js
+++ src/index.js
@@ -1,37 +1,43 @@
 // function component() {
-//   var element = document.createElement('div');
+//   var element = document.createElement('div')
 //
 //   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 //
-//   return element;
+//   return element
 // }
 //
-// document.body.appendChild(component());
+// document.body.appendChild(component())
 
-import _ from 'lodash';
-// import './style.css';
-// import Icon from './twitter.jpg';
-// import Data from './data.xml';
-
+import _ from 'lodash'
+import printMe from './print.js'
+// import './style.css'
+// import Icon from './twitter.jpg'
+// import Data from './data.xml'
 
 function component() {
-  var element = document.createElement('div');
+    var element = document.createElement('div')
+    var btn = document.createElement('button')
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.classList.add('hello');
+    // Lodash, currently included via a script, is required for this line to work
+    // Lodash, now imported by this script
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-     // Add the image to our existing div.
-   // var myIcon = new Image();
-   // myIcon.src = Icon;
-   //
-   // element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
 
-   // console.log(Data);
+    element.appendChild(btn)
+        // element.classList.add('hello')
 
-  return element;
+    // Add the image to our existing div.
+    // var myIcon = new Image()
+    // myIcon.src = Icon
+    //
+    // element.appendChild(myIcon)
+
+    // console.log(Data)
+
+    return element
 }
 
-document.body.appendChild(component());
+document.body.appendChild(component())
