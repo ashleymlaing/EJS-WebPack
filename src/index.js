5beef070b8513d90e9e2292180a60c4e00c023b9
Ashley Marie Laing <ashleym.laing@gmail.com>
2018-01-04 12:49:32 -0500

    added EJS chapter 7 project in, start and stop functions are coming with errors and warnings

--- src/index.js
+++ src/index.js
@@ -11,22 +11,31 @@
 
 import _ from 'lodash'
 import printMe from './print.js'
-// import './style.css'
+import start from './print.js'
+import stop from './print.js'
+import './style.css'
 // import Icon from './twitter.jpg'
 // import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div')
     var btn = document.createElement('button')
+    var btn2 = document.createElement('button')
+    var element2 = document.createElement('section')
 
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ')
 
-    btn.innerHTML = 'Click me and check the console!'
-    btn.onclick = printMe
+    btn.innerHTML = 'Start'
+    btn.onclick = start
+
+    btn2.innerHTML = 'Stop'
+    btn2.onclick = stop
 
     element.appendChild(btn)
+    element.appendChild(btn2)
+    element.appendChild(element2)
         // element.classList.add('hello')
 
     // Add the image to our existing div.
@@ -40,4 +49,17 @@ function component() {
     return element
 }
 
-document.body.appendChild(component())
+// document.body.appendChild(component())
+
+let element = component(); // Store the element to re-render on print.js changes
+document.body.appendChild(element)
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!')
+            // printMe()
+        document.body.removeChild(element)
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element)
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -15,61 +15,65 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     // entry: './src/index.js',
     entry: {
-        app: './src/index.js',
-        print: './src/print.js'
+        // app: './src/index.js',
+        // print: './src/print.js'
+        app: './src/index.js'
     },
     devtool: 'inline-source-map',
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
         new HtmlWebpackPlugin({
             title: 'Output Management'
-        })
+        }),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
     ],
     output: {
         // filename: 'bundle.js',
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    // module: {
-    //   rules: [
-    //     {
-    //       test: /\.css$/,
-    //       use: [
-    //         'style-loader',
-    //         'css-loader'
-    //       ]
-    //     },
-    //     {
-    //        test: /\.(png|svg|jpg|gif)$/,
-    //        use: [
-    //          'file-loader'
-    //        ]
-    //      },
-    //      {
-    //        test: /\.(woff|woff2|eot|ttf|otf)$/,
-    //        use: [
-    //          'file-loader'
-    //        ]
-    //      },
-    //     {
-    //        test: /\.(csv|tsv)$/,
-    //        use: [
-    //         'csv-loader'
-    //        ]
-    //      },
-    //      {
-    //        test: /\.xml$/,
-    //        use: [
-    //          'xml-loader'
-    //        ]
-    //      }
-    //   ]
-    // }
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            //     {
+            //        test: /\.(png|svg|jpg|gif)$/,
+            //        use: [
+            //          'file-loader'
+            //        ]
+            //      },
+            //      {
+            //        test: /\.(woff|woff2|eot|ttf|otf)$/,
+            //        use: [
+            //          'file-loader'
+            //        ]
+            //      },
+            //     {
+            //        test: /\.(csv|tsv)$/,
+            //        use: [
+            //         'csv-loader'
+            //        ]
+            //      },
+            //      {
+            //        test: /\.xml$/,
+            //        use: [
+            //          'xml-loader'
+            //        ]
+            //      }
+        ]
+    }
 }
